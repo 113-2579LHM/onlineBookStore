797602a6e159b91218a4d6655889878b62568651
113-2579LHM <cvn6enterprise@outlook.com>
2024-12-26 21:35:30 +0800

    修改登录的js，完成登录的验证；修改注册功能密码为加密存放；将js移动到独立的文件中

---
+++ js/login.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    'use strict';
+
+    $('.form-validate').each(function() {  
+        $(this).validate({
+            errorElement: "div",
+            errorClass: 'is-invalid',
+            validClass: 'is-valid',
+            ignore: ':hidden:not(.summernote, .checkbox-template, .form-control-custom),.note-editable.card-block',
+            errorPlacement: function (error, element) {
+                // Add the `invalid-feedback` class to the error element
+                error.addClass("invalid-feedback");
+                console.log(element);
+                if (element.prop("type") === "checkbox") {
+                    error.insertAfter(element.siblings("label"));
+                } 
+                else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+
+    });    
+
+    // ------------------------------------------------------- //
+    // Material Inputs
+    // ------------------------------------------------------ //
+
+    var materialInputs = $('input.input-material');
+
+    // activate labels for prefilled values
+    materialInputs.filter(function() { return $(this).val() !== ""; }).siblings('.label-material').addClass('active');
+
+    // move label on focus
+    materialInputs.on('focus', function () {
+        $(this).siblings('.label-material').addClass('active');
+    });
+
+    // remove/keep label on blur
+    materialInputs.on('blur', function () {
+        $(this).siblings('.label-material').removeClass('active');
+
+        if ($(this).val() !== '') {
+            $(this).siblings('.label-material').addClass('active');
+        } else {
+            $(this).siblings('.label-material').removeClass('active');
+        }
+    });
+
+
+    $(function () {
+        /*登录*/
+        $("#login").click(async function () {
+          console.log("login");
+          var userName = $("#login-username").val();
+          var passWord = $("#login-password").val();
+  
+          const hashedPassword = await hashPassword(passWord);
+  
+          /*获取当前输入的账号密码*/
+          const storedPassword = localStorage.getItem('user.' + userName);
+          if (storedPassword && storedPassword === hashedPassword) {
+            alert("登录成功");
+            location = "/";
+          } else {
+            alert("用户名或密码错误");
+            $("#login-username").addClass("form-control is-invalid");
+            $("#login-password").addClass("form-control is-invalid");
+          }
+      })
+  
+      async function hashPassword(password) {
+        const msgUint8 = new TextEncoder().encode(password);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+      }
+      });
+});
